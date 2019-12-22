ba487fbb465d45245f09938b49d32e4480714cc5
healer111458 <1114581211@qq.com>
2019-12-22 18:08:05 +0800

    'del'

--- uml/trip/index.js
+++ uml/trip/index.js
@@ -21,12 +21,12 @@ class Trip {
     constructor(car, length) {
         this.car = car;
         this.length = length;
-        start() {
-            console.log(`行程开始了:名称${this.car.name},车牌号：${this.car.number}`);
-        }
-        encodeURI() {
-            console.log(`行程结束，价钱是：${this.car.price*this.car.length}元`);
-        }
+        // start() {
+        //     console.log(`行程开始了:名称${this.car.name},车牌号：${this.car.number}`);
+        // }
+        // end() {
+        //     console.log(`行程结束，价钱是：${this.car.price*this.car.length}元`);
+        // }
     }
 }
 let car = new Kuaiche('京A88888', '宝马x6');
