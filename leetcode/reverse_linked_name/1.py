1382d95f6ddfd904a40a93c281842c76cb02f3ab
healer111458 <1114581211@qq.com>
2020-01-14 21:54:44 +0800

    LeetCode反转链表

---
+++ leetcode/reverse_linked_name/1.py
@@ -0,0 +1,14 @@
+Definition for singly-linked list.
+ class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head: return None
+        prev = None #前趋结点
+        cur = head #当前结点
+        while cur:
+            cur.next,prev,cur = prev,cur,cur.next
+        return prev
