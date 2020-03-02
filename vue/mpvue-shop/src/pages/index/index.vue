61769f0a0d52def7756c2eb77feb440d0779aed0
healer111458 <1114581211@qq.com>
2020-03-02 20:02:29 +0800

    仿网易严选wxapp

---
+++ vue/mpvue-shop/src/pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="index">
+    <!-- 头部的搜索 -->
+    <div class="search">
+      <div @click="toMappage">{{cityName}}</div>
+      <div>
+        <input type="text" placeholder="搜索商品" />
+        <span class="icon"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import amapFile from '../../utils/amap-wx.js'
+export default {
+  data () {
+    return{
+      cityName: '南昌'
+    }
+  },
+methods: {
+  toMappage() {
+    //通过 wx.getSetting 查询是否授权 "scoped.record"
+    let _this =this
+    wx.getSetting({
+      success: (res) => {
+        //如果没有授权 请求获得授权
+        // console.log(res)
+        if(!res.authSetting['scope.userLocation']) {
+          wx.openSetting({
+            success:res => {
+              //请求获得授权
+              _this.getCityNmae()
+            }
+          })
+        }else {
+          wx.navgateTo({
+            url:'/pages/mappage/main',
+          });
+          // _this.getCityNmae()
+        }
+      },
+      fail: (err) => {
+        console.log(err)
+      },
+      complete: () => {}
+    });
+  },
+  getCityNmae () {
+    let _this = this
+    var myAmapFun = new amapFile.AMapWX({key:'4e2273031ffdccfca4d7892f173580c3'});
+    myAmapFun.getRegeo({
+      success: function (data) {
+        //成功回调
+        console.log(data)
+        //获取地理位置
+      },
+      fail:function(info) {
+        //失败回调
+        console.log(info)
+        _this.cityName = '北京'
+      }
+    })  
+  }
+}
+}
+</script>
+
+<style lang="less" scoped>
+@import "./style.less";
+</style>
---
+++ vue/mpvue-shop/src/pages/mappage/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mappage">
+      <div class="section">
+          <input type="text" placeholder="搜素" focus="ture" />
+      </div>
+      <scroll-view :scroll-y="ture" class="addcont" style="height:500rpx;">
+      <div class="ressult" v-for="(item,index) in tips" :key="index">
+          {{item.name}}
+      </div>
+      </scroll-view>
+      <div class="map_container">
+          <div class="title">显示当前位置:</div>
+          <map class="map" id="map" scale="16" :longitude="longitude" :latitude="latitude" :markers="markers"></map>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            tips: [],
+            longitude: 0,
+            latitude: 0,
+            markers: []
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
