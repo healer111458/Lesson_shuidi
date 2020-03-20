6e3c50e101e4bf70ee60a463b4b41a68b09194b0
healer111458 <1114581211@qq.com>
2020-03-20 21:16:15 +0800

    轮播图

--- vue/mpvue-shop-node/controllers/index.js
+++ vue/mpvue-shop-node/controllers/index.js
@@ -2,27 +2,27 @@ const _ = require('lodash')
 const fs = require('fs')
 const path = require('path')
 
-//映射  d 将某文件夹下的文件为模块使用
+// 映射 d 文件夹下的文件为模块
 
 const mapDir = d => {
-    const tree = {}
+  const tree = {}
 
-    //获取当前文件夹下的所有文件夹和文件  分成两组：一组文件夹，一组文件
-    const [dirs,files] = _(fs.readdirSync(d)).partition(p => fs.statSync(path.join(d,p)).isDirectory())       
-    //partion：划分
-    //返回true ->映射文件夹
-    dirs.forEach(dir => {
-        tree[dir] = mapDir(path.join(d,dir))
-    });
-    //映射文件
-    files.forEach(file => {
-        //获取文件后缀名
-        if(path.extname(file) === '.js') {
-            tree[path.basename(file,'.js')] = require(path.join(d,file))
-        }
-    })
+  // 获取当前文件夹下的所有文件夹和文件，分成两组，文件夹一组，文件一组
+  const [dirs, files] = _(fs.readdirSync(d)).partition(p => fs.statSync(path.join(d, p)).isDirectory())
+  // 映射文件夹
+  dirs.forEach(dir => {
+    tree[dir] = mapDir(path.join(d, dir))
+  });
+  // 映射文件
+  files.forEach(file => {
+    // 获取文件后缀名字
+    if (path.extname(file) === '.js') {
+      tree[path.basename(file, '.js')] = require(path.join(d, file))
+    }
+  })
 
-    return tree
+  return tree
 }
-//默认导出当前文件夹的映射
+
+// 默认导出当前文件夹下的映射
 module.exports = mapDir(path.join(__dirname))
