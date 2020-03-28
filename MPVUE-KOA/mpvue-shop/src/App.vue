a35bf6d7e5c6457ac00d94deb4ca02ca598e2e68
healer111458 <1114581211@qq.com>
2020-03-28 21:33:41 +0800

    首页微调

--- MPVUE-KOA/mpvue-shop/src/App.vue
+++ MPVUE-KOA/mpvue-shop/src/App.vue
@@ -2,6 +2,20 @@
 export default {
   created () {
     // 调用API从本地缓存中获取数据
+    var userInfo = {
+      openId: "oQmbb4sNZdxaUQZ0sfYgvtOP2S7c",
+      nickName: "蜗牛",
+      gender: 1,
+      language: "zh_CN",
+      city: "Changping",
+      province: "Beijing",
+      country: "China",
+      avatarUrl: "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTIbWFEIJj8IpGeHM7dGic1aTFZALjWcMm9ltWfFiaQfVRYticWBfgGfzXWMt2EkJWiaicPtftHAlWxUibxQ/132",
+      watermark: { timestamp: 1535513485, appid: "wx601ce71bde7b9add" }
+    };
+    var openId = userInfo.openId;
+    wx.setStorageSync("userInfo", userInfo);
+    wx.setStorageSync("openId", openId);
     /*
      * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
      * 微信：mpvue === wx, mpvuePlatform === 'wx'
@@ -40,11 +54,28 @@ export default {
   padding: 200rpx 0;
   box-sizing: border-box;
 }
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
+page{
+  background: #f4f4f4;
+  height: 100%;
+}
+button{
+  background: none;
+  padding: 0;
+  font-weight: normal;
+  font-size: 32rpx;
+  box-sizing: content-box;
+}
+button::after{
+  border: 0;
+}
+view,text{
+  font-size: 28rpx;
+  color: #333333;
+}
+.wxParse .p{
+  margin: 0 !important;
+}
+.wxParse .img{
+  display: block !important;
 }
 </style>
