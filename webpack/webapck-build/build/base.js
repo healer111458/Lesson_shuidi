6badebd18d4ad5624e9d4d7ddbddd1caae1d33d0
healer111458 <1114581211@qq.com>
2020-04-10 17:03:49 +0800

    webpack

--- webpack/webapck-build/build/base.js
+++ webpack/webapck-build/build/base.js
@@ -10,16 +10,21 @@ console.log(files, '+++++++++++++++');
 
 module.exports = () => {
   // console.log('在这里做基本配置吧');
-  config
-    .entry('app')
-      .add(resolve('src/main.js'))
-      .end()
-    .set('mode', process.env.NODE_ENV)
-    .output
-      .path(resolve('dist'))
-      .filename('[name].bundle.js');
-
+  // files 都  webpack config 化？ 
+  // 可拔插
+  const map = new Map(); // es6 Map  json 区别 key 不限类型
+  // 重新files
+  files.map(file => {
+    const name =  file.split('/').pop().replace('.js', '');
+    // console.log(name, '-------')
+    //path babelLoader.js require 引入  闭包函数
+    // console.log(require(file).toString(), '|||||||||||||');
+    return map.set(name, require(file)(config, resolve));
+  })
+  // console.log(map, 'map------------------')
   // webpack  modules 
+  // 分模块的配置就上去了
+  map.forEach(v => v()) // 运行了配置
   
   return config;
 }
