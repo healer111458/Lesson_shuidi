ad2d30a9e4bbc05d712f500e5ed1e221cd5a5225
healer111458 <1114581211@qq.com>
2020-04-18 21:10:28 +0800

    vue

--- Healer/healer/src/App.vue
+++ Healer/healer/src/App.vue
@@ -2,6 +2,10 @@
   <div class="head">
     <img src="./assets/healer.jpg" alt="一张治愈的图片">
   </div>
+  <div class="body">
+    <button id="start">开始</button>
+    <button id="end">结束</button>
+  </div>
 </template>
 
 <script>
