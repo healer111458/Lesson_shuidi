e323e298458a5b8b66fb7bca2c37b3251ad6e8d3
healer111458 <1114581211@qq.com>
2020-04-22 17:10:27 +0800

    three.js与react

---
+++ webpack/react-three2/src/client.js
@@ -0,0 +1,12 @@
+// console.log('dfsdf');
+// var a = 1
+// document.body.innerHTML = a
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+// react 组件分为很多种  容器型组件  redux 
+import GameContainer from './containers/GameContainer'
+
+ReactDOM.render(
+  <GameContainer/>,
+  document.getElementById('game')
+)
