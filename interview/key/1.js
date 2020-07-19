9f723b21cc3101ec6bddae95c381a09bd4cf9fc5
healer111458 <1114581211@qq.com>
2020-07-19 19:38:26 +0800

    递归、循环(迭代)

---
+++ interview/key/1.js
@@ -0,0 +1,7 @@
+const sweetArray = [2, 3, 4, 5, 35]
+const sweeterArray = sweetArray.map(sweetItem => {
+    return sweetItem * 2
+})
+ 
+console.log(sweetArray)   // [2, 3, 4, 5, 35]
+console.log(sweeterArray) // [4, 6, 8, 10, 70]
