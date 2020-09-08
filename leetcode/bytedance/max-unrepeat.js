0e0af3de99448f790b6b86fe5e2e2bc1745f596f
healer111458 <1114581211@qq.com>
2020-09-08 17:29:47 +0800

    bytedance算法训练

---
+++ leetcode/bytedance/max-unrepeat.js
@@ -0,0 +1,14 @@
+var lengthOfLongestSubstring = function(s) {
+    var ls = s.charAt(0);
+    var length = ls.length;
+    for(i=1;i <= s.length;i++){
+        let index = -1;
+        index = ls.indexOf(s.charAt(i))
+        if(index > -1){
+            length = ls.length > length ? ls.length : length;
+            ls = ls.substr(index+1,s.length);
+        }
+        ls += s.charAt(i);
+    }
+    return length;
+};
